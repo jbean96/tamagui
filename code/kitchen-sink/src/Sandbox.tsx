d42ddfb011d4a4d1383f77416d2b7edf5c702d15
Joshua Bean <josh@arrivedhomes.com>
2024-06-17 13:35:48 -0700

    repro of viewport styles of custom variants not applying to children

--- code/kitchen-sink/src/Sandbox.tsx
+++ code/kitchen-sink/src/Sandbox.tsx
@@ -1,29 +1,54 @@
 //! tamagui-ignore
 //! debug-verbose
-import './wdyr'
+import "./wdyr";
 
-import { AnimatePresence, Button, Circle, Square, YStack, styled } from 'tamagui'
-// import { DatePickerExample } from '../../bento/src/components/elements/datepickers/DatePicker'
+import {View as RNView} from "react-native";
+import {Circle, YStack, createStyledContext, styled} from "tamagui";
 
-import { useState } from 'react'
-import { View as RNView } from 'react-native'
+const StyledStackContext = createStyledContext({condensed: true});
+
+const StyledStack = styled(YStack, {
+  context: StyledStackContext,
+  name: "StyledStack",
+
+  bg: "#AAAAAA",
+  p: "$4",
+
+  variants: {
+    condensed: {
+      false: {
+        p: "$10",
+        bg: "#DDDDDD",
+      },
+    },
+  } as const,
+  defaultVariants: {
+    condensed: true,
+  },
+});
+
+const StyledCircle = styled(Circle, {
+  context: StyledStackContext,
+  name: "StyledStackCircle",
+
+  bg: "red",
+  size: 100,
+
+  variants: {
+    condensed: {
+      false: {
+        bg: "blue",
+      },
+    },
+  } as const,
+});
 
 export const Sandbox = () => {
   return (
-    <RNView style={{ width: '100%', height: '100%', padding: 50 }}>
-      {/* <Demo3 /> */}
-      {/* <Circle
-        debug="verbose"
-        size={100}
-        bg="red"
-        animation="bouncy"
-        enterStyle={{
-          // opacity: 0,
-          y: -100,
-        }}
-      /> */}
-
-      {/* <DatePickerExample /> */}
+    <RNView style={{width: "100%", height: "100%", padding: 50}}>
+      <StyledStack $gtXs={{condensed: false}}>
+        <StyledCircle />
+      </StyledStack>
     </RNView>
-  )
-}
+  );
+};
