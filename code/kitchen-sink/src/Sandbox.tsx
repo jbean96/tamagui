4d9e8fc3413594e95358aae4ffb7824c2c13f644
Joshua Bean <josh@arrivedhomes.com>
2024-07-12 15:59:34 -0700

    group-hover bug repro

--- code/kitchen-sink/src/Sandbox.tsx
+++ code/kitchen-sink/src/Sandbox.tsx
@@ -1,32 +1,62 @@
 //! tamagui-ignore
 //! debug-verbose
-import './wdyr'
+import "./wdyr";
 
-import { AnimatePresence, Button, Circle, Square, YStack, styled } from 'tamagui'
+import {SliderDemo} from "@tamagui/demos";
+import {useState} from "react";
+import {View as RNView} from "react-native";
+import {
+  AnimatePresence,
+  Button,
+  Circle,
+  Square,
+  Stack,
+  Text,
+  YStack,
+  styled,
+} from "tamagui";
+
 // import { DatePickerExample } from '../../bento/src/components/elements/datepickers/DatePicker'
 
-import { useState } from 'react'
-import { View as RNView } from 'react-native'
-import { SliderDemo } from '@tamagui/demos'
+const Row = styled(Stack, {
+  tag: "button",
+  flexDirection: "row",
+  alignItems: "center",
+  minWidth: 300,
+  alignSelf: "flex-start",
+  p: "$2",
+  cursor: "pointer",
+
+  bg: "#DDDDDD",
+
+  hoverStyle: {
+    bg: "#DDFFDD",
+  },
+
+  pressStyle: {
+    bg: "#FFDDDD",
+  },
+
+  group: "Row",
+});
+
+const RowText = styled(Text, {
+  "$group-Row-hover": {
+    color: "#FF00FF",
+    pl: "$2",
+  },
+
+  "$group-Row-press": {
+    color: "#00FFFF",
+  },
+});
 
 export const Sandbox = () => {
   return (
-    <RNView style={{ width: '100%', height: '100%', padding: 50 }}>
-      {/* <Demo3 /> */}
-      {/* <Circle
-        debug="verbose"
-        size={100}
-        bg="red"
-        animation="bouncy"
-        enterStyle={{
-          // opacity: 0,
-          y: -100,
-        }}
-      /> */}
-
-      <SliderDemo />
-
-      {/* <DatePickerExample /> */}
+    <RNView style={{width: "100%", height: "100%", padding: 50}}>
+      <Row>
+        <RowText>Hello</RowText>
+      </Row>
     </RNView>
-  )
-}
+  );
+};
